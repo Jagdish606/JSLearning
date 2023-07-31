5b25dc0249aa380690a0aaf02257b8344123e531
Jagdish606 <jagdishdesai2001@gmail.com>
2023-07-31 10:15:42 +0530

    loops
    loops

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,28 @@
+console.log("Number from 5 to 50 incrementing by 1");
+for (let index = 5; index <= 15; index=index+1) {
+    console.log(index);
+}
+console.log("Number from 50 to 40 decrementing by 1");
+for (let index = 50; index >= 40; index=index-1) {
+    console.log(index);
+}
+console.log("Find first 15 odd numbers");
+for (let index = 1; index < 10; index=index+2) {
+    console.log(index);
+}
+console.log("first 10 even numbers");
+for (let index = 0; index < 20; index=index+2) {
+    console.log(index);
+}
+console.log("table of 5");
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+console.log("table of 10");
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index);
+}
+console.log("reverse order");
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+}
---
+++ 06_while_Assignment01.js
@@ -0,0 +1,46 @@
+console.log("Number from 5 to 50 incrementing by 1");
+var i=5;
+while (i <= 15) {
+    console.log(i);
+    i=i+1;
+}
+console.log("Number from 50 to 40 decrementing by 1");
+var i=50;
+while (i >=40) {
+    console.log(i);
+    i=i-1;
+}
+console.log("Find first 15 odd numbers");
+var i=1;
+while (i < 11) {
+    console.log(i);
+    i=i+2;
+}
+console.log("first 10 even numbers");
+var i=0;
+while (i<20) {
+    console.log(i);
+    i=i+2;
+    
+}
+console.log("table of 5");
+var i=5;
+while (i<=50) {
+    console.log(i);
+    i=i+5;
+    
+}
+console.log("table of 10");
+var i=10;
+while (i<=100) {
+    console.log(i);
+    i=i+10;
+    
+}
+console.log("reverse order");
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i=i-10;
+    
+}
