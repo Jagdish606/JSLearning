aae6547e0c61c91cf4b7b9ec07e492b23aeea58c
Jagdish606 <jagdishdesai2001@gmail.com>
2023-08-10 07:07:06 +0530

    object cloning
    object cloning

--- 10_objectClone.js
+++ 10_objectClone.js
@@ -41,18 +41,3 @@ console.log(` Elon Details: ${studentElon.fullName} `);
 console.log(` Bill Details: ${studentBill.fullName}`);
 
 
-
-let jenny = {
-    fullName: "Jenny Musk",
-    age: 22,
-    city: "Pune",
-    college: "COEP Pune", 
-}
-marks = {
-    physics: 100,
-    science: 99,
-    math: 92
-}
-
-let mergedObject = Object.assign({} , jenny, marks);
-console.table(mergedObject);
